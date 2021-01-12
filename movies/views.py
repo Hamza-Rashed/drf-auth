ef02868597fbe2ba7cf4e39eaf6a9be600f1adbe
Hamza-Rashed <hamzarashed503@gmail.com>
2021-01-12 20:20:38 +0200

    first commit

---
+++ movies/views.py
@@ -0,0 +1,16 @@
+
+from django.shortcuts import render
+from rest_framework import generics
+from .permissions import IsAuthorOrReadOnly
+from .models import Movies
+from .serializer import MoviesSerializer
+
+class MoviesList(generics.ListCreateAPIView):
+    queryset = Movies.objects.all()
+    serializer_class = MoviesSerializer
+
+class MovieDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movies.objects.all()
+    serializer_class = MoviesSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
